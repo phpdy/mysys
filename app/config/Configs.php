53dcdfeb80a60f46366918505975d1e532fded75
dyong <dyong525@163.com>
2014-01-19 20:19:47 +0800

    cp

--- app/config/Configs.php
+++ app/config/Configs.php
@@ -15,7 +15,7 @@ class Configs{
 			'charset'	=> 'utf8'
 		),
 		"ali"		=>	array(
-			'mysql_conn' => 'mysql:host=localhost;port=3306;dbname=sys' ,
+			'mysql_conn' => 'mysql:host=localhost;port=3306;dbname=ali' ,
 			'mysql_user' => 'root',
 			'mysql_pwd' => 'root',
 			'charset'	=> 'utf8'
